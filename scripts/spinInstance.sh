969e6a862b2a3526dba180da19e38d51ad6d37f9
mbali17 <alimirbasheer@gmail.com>
2018-04-08 15:35:28 -0500

    removing network details

--- scripts/spinInstance.sh
+++ scripts/spinInstance.sh
@@ -1,5 +1,5 @@
 #Using bash shell.
 #!/bin/sh
 echo "Spinning an EC2 Instance"
-aws ec2 run-instances --image-id ami-d874e0a0 --count 1 --instance-type t2.micro --key-name certprep --security-group-ids sg-e44fa39e \
---subnet-id subnet-d0c1628b --region us-west-2 --user-data file://UserDataScript.txt
+aws ec2 run-instances --image-id ami-d874e0a0 --count 1 --instance-type t2.micro --key-name  --security-group-ids  \
+--subnet-id  --region  --user-data file://UserDataScript.txt
